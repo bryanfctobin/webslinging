28257a092ce5c66a565eba095e4fceb99bf5c1f3
Bryan Tobin <bryanfctobin@gmail.com>
2021-04-17 23:40:25 -0400

    Working version

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-var b = axios.get("https://bryantobin.com").then(function (response) {
-  console.log(response.data);
+axios.get("https://bryantobin.com").then(function (response) {
+  let $ = cheerio.load(response.data);
+  console.log($("#ssb").text());
 });
