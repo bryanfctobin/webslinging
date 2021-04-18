2730adc97be06b35d638e690dc36cef60ef83ab4
Bryan Tobin <bryanfctobin@gmail.com>
2021-04-17 22:38:20 -0400

    Axios web request

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-// var scraped_data = await axios.get("https://books.toscrape.com/");
-var scraped_data = "<h1>Hello</h1>";
-const $ = cheerio.load("https://bryantobin.com");
-// var name = $(".page_inner").first().find("a").text();
-console.log(scraped_data);
+var b = axios.get("https://bryantobin.com").then(function (response) {
+  console.log(response.data);
+});
